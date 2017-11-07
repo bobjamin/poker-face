45a1aabfce78c8e64a93c25a08a3a332cfea4e8a
Chris Barbour <cbarbour02@qub.ac.uk>
2017-11-07 07:58:15 +0000

    added more tests to printer and updated readme with build and run info

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Poker Face
+The poker-face project runs as a command line tool and will print out various Poker hand ranks when given 5 card hands.
+
+## Build
+To build the project, clone the repo and run `mvn package`. This will create a runnable jar named `poker-face.jar` in the `target` directory.
+
+## Run
+Once built you can run the project with the command `java -jar target/poker-face.jar <<args>>` where `<<args>>` can be replaed with either:
+1. 5 separate card strings or
+2. A location of a file containing multiple poker hands
--- src/test/kotlin/pokerface/hand/PokerHandPrinterTest.kt
+++ src/test/kotlin/pokerface/hand/PokerHandPrinterTest.kt
@@ -1,22 +1,16 @@
 package pokerface.hand
 
 import junit.framework.TestCase.assertEquals
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.ExpectedException
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
 class PokerHandPrinterTest {
 
-    @Rule
-    @JvmField
-    var exceptions = ExpectedException.none()
-
     @Test
     fun `should print full set of valid hands from file`(){
         val testOutputStream = ByteArrayOutputStream()
-        val validPokerHandsFile = PokerHandPrinterTest::class.java.classLoader.getResourceAsStream("validPokerHands.txt");
+        val validPokerHandsFile = PokerHandPrinterTest::class.java.classLoader.getResourceAsStream("validPokerHands.txt")
         PokerHandPrinter.printHandRanksFrom(validPokerHandsFile, PrintStream(testOutputStream))
         val expectedOutput =
                 "AS KS QS JS TS -> Royal flush\n" +
@@ -30,5 +24,30 @@ class PokerHandPrinterTest {
                 "AC AD 7C 9C 3C -> One pair\n" +
                 "2D 8D 9C TS KH -> High card\n"
         assertEquals(expectedOutput, testOutputStream.toString())
+        testOutputStream.close()
+    }
+
+    @Test
+    fun `should succeed when there are errors and print the errors to errorStream`(){
+        val testOutputStream = ByteArrayOutputStream()
+        val testErrorStream = ByteArrayOutputStream()
+        val validPokerHandsFile = PokerHandPrinterTest::class.java.classLoader.getResourceAsStream("invalidPokerHands.txt")
+        PokerHandPrinter.printHandRanksFrom(validPokerHandsFile, PrintStream(testOutputStream), PrintStream(testErrorStream))
+        val expectedSuccessOutput =
+                "AS KS QS JS TS -> Royal flush\n" +
+                "3C 3D 3H 3S 4D -> Four of a kind\n" +
+                "2C 4C 9C TC QC -> Flush\n" +
+                "4H 4D 4S AC KD -> Three of a kind\n" +
+                "7C 7S KD KS QH -> Two pair\n" +
+                "2D 8D 9C TS KH -> High card\n"
+        val expectedErrorOutput =
+                "AH 2H 1H 4H 5H -> ERROR: Cannot parse Poker hand because Character '1' is not a valid card value\n" +
+                "7P 7D 7C AS AC -> ERROR: Cannot parse Poker hand because Character 'P' is not a valid suit value\n" +
+                "AH JS QD TD IS -> ERROR: Cannot parse Poker hand because Character 'I' is not a valid card value\n" +
+                "AC AD 7C 9C 10C -> ERROR: Cannot parse string 'AC AD 7C 9C 10C', must be exactly 14 characters long\n"
+        assertEquals(expectedSuccessOutput, testOutputStream.toString())
+        assertEquals(expectedErrorOutput, testErrorStream.toString())
+        testOutputStream.close()
+        testErrorStream.close()
     }
 }
